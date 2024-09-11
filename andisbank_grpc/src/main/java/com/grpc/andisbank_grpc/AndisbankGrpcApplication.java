bfbc57d6340ebde126efce54a0ce5eb8f13d3762
Lucas Giffuni <55067106+LucasGiffuni@users.noreply.github.com>
2024-09-11 18:30:16 -0300

    GRPC

---
+++ andisbank_grpc/src/main/java/com/grpc/andisbank_grpc/AndisbankGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.grpc.andisbank_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndisbankGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndisbankGrpcApplication.class, args);
+	}
+
+}
---
+++ andisbank_grpc/src/test/java/com/grpc/andisbank_grpc/AndisbankGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpc.andisbank_grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndisbankGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
